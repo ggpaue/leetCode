7e40665b8b51c8775652446990c091e0fb644187
ggpaue <ggpaue@gmail.com>
2013-08-10 22:45:44 -0400

    BalancedBinaryTree

---
+++ BalancedBinaryTree.java
@@ -0,0 +1,13 @@
+public class Solution {
+	public boolean isBalanced(TreeNode root) {
+		if(root == null) return true;
+		int left = getHeight(root.left);
+		int right = getHeight(root.right);
+		if(Math.abs(left - right) > 1) return false;
+		return isBalanced(root.left) && isBalanced(root.right);
+	}
+	private int getHeight(TreeNode n) {
+		if(n == null) return 0;
+		return Math.max(getHeight(n.left), getHeight(n.right)) + 1;
+	}
+}

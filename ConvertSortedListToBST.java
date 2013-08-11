baa942f919e6af132add7151cf4fa887edaba31e
ggpaue <ggpaue@gmail.com>
2013-08-10 23:25:49 -0400

    Update ConvertSortedListToBST.java

--- ConvertSortedListToBST.java
+++ ConvertSortedListToBST.java
@@ -1,3 +1,25 @@
+/**
+Convert Sorted List to Binary Search Tree
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+**/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
 public class Solution {
     
 	public int calLen(ListNode head) {

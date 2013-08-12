6672b5ef7558790f8fb3a4eb3491214ae4fd8ea5
ggpaue <ggpaue@gmail.com>
2013-08-12 16:20:05 -0400

    PalindromePartitioning

---
+++ PalindromePartitioning.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Solution {
+	public ArrayList<ArrayList<String>> partition(String s) {
+		ArrayList<ArrayList<String>> results = new ArrayList<ArrayList<String>>();
+		int length = s.length();
+		if(length == 0) return results;
+		ArrayList<String> pt = new ArrayList<String>();
+		findPartition(s, 0, pt, results);
+		return results;
+	}
+	void findPartition(String s, int start, ArrayList<String> pt, ArrayList<ArrayList<String>> results) {
+		if(start >= s.length()) {
+			ArrayList<String> copy = new ArrayList<String>();
+			for(int j=0; j<pt.size(); j++) {
+				copy.add(pt.get(j));
+			}
+			results.add(copy);
+		}
+		for (int i = start; i<s.length(); i++) {
+			if(isPalindrome(s, start, i)) {
+				pt.add(s.substring(start, i+1));
+				findPartition(s, i+1, pt, results);
+				pt.remove(pt.size() - 1);
+			}
+		}
+	}
+	boolean isPalindrome(String s, int begin, int end) {
+		while(begin < end) {
+			if(s.charAt(begin) != s.charAt(end)) {
+				return false;
+			}
+			begin++;
+			end--;
+		}
+		return true;
+	}
+
+}

8073ff7ba4e94a49371e21abc6611fbbca27d3b8
ggpaue <ggpaue@gmail.com>
2013-08-14 18:15:33 -0400

    JumpGame

--- JumpGame.java
+++ JumpGame.java
@@ -1,5 +1,5 @@
 /**
-Jump GameMar 25 '12
+Jump Game
 Given an array of non-negative integers, you are initially positioned at the first index of the array.
 
 Each element in the array represents your maximum jump length at that position.

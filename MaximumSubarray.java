8dac7043c050f3ecf5b557ac17490ef5afe02819
ggpaue <ggpaue@gmail.com>
2013-08-18 12:10:29 -0400

    MaximumSubarray

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,4 +1,24 @@
 
 public class Solution {
+	public int maxSubArray(int[] A) {
+		if(A == null || A.length == 0) {
+			return 0;
+		} else if(A.length == 1) {
+			return A[0];
+		}
+		int max = Integer.MIN_VALUE;
+		int currSum = 0;
+		int idx = 0;
+		while(idx < A.length) {
+			currSum += A[idx];
+			max = currSum > max ? currSum : max;
+			if(currSum < 0) {
+				currSum = 0;
+			}
+			idx++;
+		}
+		return max;
+		
+	}
 
 }

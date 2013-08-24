a8ea88a9e6e060e63a765c4cb26fcca1a8dea51f
ggpaue <ggpaue@gmail.com>
2013-08-23 22:05:19 -0400

    WordLadder

---
+++ WordLadder.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ * Given two words (start and end), and a dictionary, find the length of shortest transformation sequence from start to end, such that:
+
+ * Only one letter can be changed at a time
+ * Each intermediate word must exist in the dictionary
+ * For example,
+
+ * Given:
+ * start = "hit"
+ * end = "cog"
+ * dict = ["hot","dot","dog","lot","log"]
+
+ * As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+ * return its length 5.
+
+ * Note:
+
+ * Return 0 if there is no such transformation sequence.
+ * All words have the same length.
+ * All words contain only lowercase alphabetic characters.
+ * 
+ * 
+ */
+
+
+import java.util.*;
+public class Solution {
+	public int ladderLength(String start, String end, HashSet<String> dict) {
+		
+		
+		Set<String> set = new HashSet<String>();
+		Queue<String> queue = new LinkedList<String>();
+		queue.offer(start);
+		int distance = 1;
+		int count = 1;
+		set.add(start);
+		
+		while(count > 0) {
+			while(count > 0) {
+				char[] curr = queue.poll().toCharArray();
+				for(int i = 0; i < curr.length; i++) {
+					char tmp = curr[i];
+					for(char c = 'a'; c <= 'z'; c++) {
+						if(c == tmp) continue;
+						curr[i] = c;
+						String str = new String(curr);
+						if(str.equals(end)) return distance + 1;
+						if(dict.contains(str) && !set.contains(str)) {
+							queue.offer(str);
+							set.add(str);
+						}
+					}
+					curr[i] = tmp;
+				}
+				count--;
+			}
+			distance++;
+			count = queue.size();
+		}
+		return 0;
+	}
+
+}

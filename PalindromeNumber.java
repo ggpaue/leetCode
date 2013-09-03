6001218d8dcd8aa93c13107267f0cb45b00808bc
Guanpeng Gao <ggpaue@gmail.com>
2013-09-03 19:32:46 -0400

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -7,19 +7,20 @@
  */
 
 public class Solution {
-	public boolean isPalindrome(int x) {
+    public boolean isPalindrome(int x) {
 		if(x < 0) return false;
-		int k = 1;
-		int t = x;
-		while((t = t / 10) != 0) k*= 10;
-		while(x >= 10) {
-			int d1 = x / k;
-			int d2 = x - x / 10 * 10;
-			if(d1 != d2) return false;
-			x = x / 10 - d1 * (k - 1);
-			k -= 2;
+		int div = 10;
+		while(x / div > 9) {
+			div *= 10;
+		}
+		while(x > 9) {
+			if(x / div != x % 10) {
+				return false;
+			} else {
+				x = x % div / 10;
+				div /= 100;
+			}
 		}
 		return true;
 	}
-
 }

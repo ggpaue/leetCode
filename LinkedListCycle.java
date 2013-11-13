4bda93c293d86b3df45a8d0d429a96ae20af5d3f
ggpaue <ggpaue@gmail.com>
2013-11-13 15:42:45 -0500

    LinkedListCycle

---
+++ LinkedListCycle.java
@@ -0,0 +1,20 @@
+
+public class Solution {
+	public boolean hasCycle(ListNode head) {
+		if(head == null) return false;
+		ListNode slow = head;
+		ListNode fast = head;
+		
+		while(true) {
+			slow = slow.next;
+			if(fast.next != null) {
+				fast = fast.next.next;
+			} else {
+				return false;
+			}
+			if(slow == null || fast == null) return false;
+			if(slow == fast) return true;
+		}
+	}
+
+}

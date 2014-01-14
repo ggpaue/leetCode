59a301005f783f7b86866b021204424b25a6963d
ggpaue <ggpaue@gmail.com>
2014-01-13 19:11:06 -0500

    LinkedListCycleII

---
+++ LinkedListCycleII.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ * @author GGPAUE
+ * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+ * 
+ * Follow up:
+ * Can you solve it without using extra space?
+ */
+public class Solution {
+	public ListNode detectCycle(ListNode head) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) break;
+        }
+        if(fast == null || fast.next == null) return null;
+        slow = head;
+        while(slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
+}

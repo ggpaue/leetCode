83b7bca1e416649eaa712f23447365487fe2352b
Guanpeng Gao <gguanpeng@vmware.com>
2020-11-05 00:07:05 -0800

    add one more solution for 46

--- python_code/46_Permutations.py
+++ python_code/46_Permutations.py
@@ -14,6 +14,7 @@
 #]
 
 from typing import List
+'''
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
@@ -21,6 +22,7 @@ class Solution:
             return res
 
         self.get_permute(res, nums, 0)
+        print(res)
         return res
 
     def get_permute(self, res, nums, index):
@@ -32,3 +34,26 @@ class Solution:
             nums[i], nums[index] = nums[index], nums[i]
             self.get_permute(res, nums, index+1)
             nums[i], nums[index] = nums[index], nums[i]
+'''
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrack(index=0):
+            if index == len(nums):
+                res.append(nums[:])
+
+            for i in range(index, len(nums)):
+                nums[i], nums[index] = nums[index], nums[i]
+                backtrack(index+1)
+                nums[i], nums[index] = nums[index], nums[i]
+
+        backtrack()
+        print(res)
+        return res
+
+
+tmp = [1,2,3]
+s = Solution()
+s.permute(tmp)

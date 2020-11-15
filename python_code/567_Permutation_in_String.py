223d20cbed8cd37b98a87dad9810249d214d14a7
Guanpeng Gao <gguanpeng@vmware.com>
2020-11-15 00:29:35 -0800

    add one more solution for 567

--- python_code/567_Permutation_in_String.py
+++ python_code/567_Permutation_in_String.py
@@ -20,7 +20,8 @@
 
 from collections import Counter
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
+    '''
+        def checkInclusion(self, s1: str, s2: str) -> bool:
         s1Counter = Counter(s1)
         s2Counter = Counter(s2[:len(s1)-1])
 
@@ -34,15 +35,47 @@ class Solution:
 
             if s2Counter[s2[start]] == 0:
                 del s2Counter[s2[start]]
-
         return False
+    '''
 
-s1= "ab"
-s2 = "eidbaooo"
-print(s2[:1])
 
-tmp1 = Counter({'i': 1, 'd': 1, 'e': 0})
-tmp2 = Counter({'i': 1, 'd': 1})
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        left, right = 0, 0
+        window = {}
+        need = {}
+        for c in s1:
+            if c not in need:
+                need[c] = 1
+            else:
+                need[c] += 1
 
+        for i in range(len(s1)):
+            c1 = s2[i]
+            if c1 not in window:
+                window[c1] = 1
+            else:
+                window[c1] += 1
+        right = len(s1)
+
+        while right < len(s2):
+            if window == need:
+                return True
+            else:
+                c1 = s2[left]
+                window[c1] -= 1
+                if window[c1] == 0:
+                    del window[c1]
+                left += 1
+
+                c2 = s2[right]
+                if c2 not in window:
+                    window[c2] = 1
+                else:
+                    window[c2] += 1
+                right += 1
+        return window == need
+
+s1= "adc"
+s2 = "dcda"
 s = Solution()
 s.checkInclusion(s1, s2)

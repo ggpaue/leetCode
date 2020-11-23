e8e763a6d2e573d31945b2612ee2a1a528b56b89
Guanpeng Gao <gguanpeng@vmware.com>
2020-11-23 14:54:42 -0800

    add one more solution for 56. Merge Intervals

--- python_code/56_Merge_Intervals.py
+++ python_code/56_Merge_Intervals.py
@@ -21,7 +21,11 @@
 from typing import List
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if intervals is None:
+        '''
+
+        :param intervals:
+        :return:
+                if intervals is None:
             return
 
         if len(intervals) <= 1:
@@ -38,6 +42,32 @@ class Solution:
             else:
                 pos += 1
         return intervals
+        '''
+
+        if intervals is None:
+            return
+
+        if len(intervals) <= 1:
+            return intervals
+
+        intervals.sort(key=lambda x: x[0])
+        res = []
+        res.append(intervals[0])
+        for i in range(1, len(intervals)):
+            cur = intervals[i]
+            last = res[-1]
+
+            if cur[0] <= last[1]:
+                last[1] = max(last[1], cur[1])
+            else:
+                res.append(cur)
+        return res
+
+
+
+
+
+
 
 test = [[1,3],[2,6],[8,10],[15,18]]
 s = Solution()

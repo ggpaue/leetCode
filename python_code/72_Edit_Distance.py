f9fe2c648416282efa60e5e2e94da05b96782a04
Guanpeng Gao <gguanpeng@vmware.com>
2020-11-26 07:19:06 -0800

    add one more solution for 72. Edit Distance

--- python_code/72_Edit_Distance.py
+++ python_code/72_Edit_Distance.py
@@ -26,6 +26,11 @@
 
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        """
+
+        :param word1:
+        :param word2:
+        :return:
         word1 = "!" + word1
         word2 = "!" + word2
 
@@ -46,9 +51,32 @@ class Solution:
                 dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+cost)
         print(dp[-1][-1])
         return int(dp[-1][-1])
+        """
+        word1 = '!' + word1
+        word2 = '!' + word2
 
-word1 = "horse"
-word2 = "ros"
+        n1 = len(word1)
+        n2 = len(word2)
+
+        dp = [[0] * n2 for _ in range(n1)]
+
+        for i in range(n1):
+            dp[i][0] = i
+
+        for j in range(n2):
+            dp[0][j] = j
+
+        for i in range(1, n1):
+            for j in range(1, n2):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+
+        return dp[-1][-1]
+
+word1 = "a"
+word2 = "b"
 
 s = Solution()
 s.minDistance(word1, word2)

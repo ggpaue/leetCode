0a40484dda84d0be548ff66191a00776e971028a
Guanpeng Gao <gguanpeng@vmware.com>
2020-11-30 23:32:16 -0800

    add one more solution for 518. Coin Change 2

--- python_code/518_Coin_Change_2.py
+++ python_code/518_Coin_Change_2.py
@@ -34,6 +34,11 @@
 from typing import List
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        '''
+
+        :param amount:
+        :param coins:
+        :return:
         n = len(coins)
         if n == 0:
             return n == amount
@@ -48,3 +53,23 @@ class Solution:
                 if i + 1 - coins[j] >= 0:
                     dp[i+1][j] += dp[i+1-coins[j]][j]
         return dp[-1][-1]
+        '''
+
+        n = len(coins)
+
+        if n == 0:
+            return int(n == amount)
+
+        dp = [[0 for i in range(amount+1)] for _ in range(n+1)]
+
+        for i in range(n+1):
+            dp[i][0] = 1
+
+        for i in range(1, n+1):
+            for j in range(1, amount+1):
+                if j - coins[i-1] >= 0:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        return dp[n][amount]
+

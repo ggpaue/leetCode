9bb5fcb57feadcae11fba7d4db464e7aeaae10c6
Guanpeng Gao <gguanpeng@vmware.com>
2021-01-13 12:17:53 -0800

    add new solution for 712. Minimum ASCII delete sum for two strings

--- python_code/712_Minimum_ASCII_Delete_Sum_for_Two_Strings.py
+++ python_code/712_Minimum_ASCII_Delete_Sum_for_Two_Strings.py
@@ -20,6 +20,12 @@
 
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        '''
+
+        :param s1:
+        :param s2:
+        :return:
+
         dp = [[0 for i in range(len(s2)+1)] for _ in range(len(s1)+1)]
 
         for i in range(len(s1)-1, -1, -1):
@@ -35,6 +41,20 @@ class Solution:
                 else:
                     dp[i][j] = min(dp[i+1][j]+ord(s1[i]), dp[i][j+1]+ord(s2[j]))
         return dp[0][0]
+        '''
+
+
+        m = len(s1)
+        n = len(s2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + ord(s1[i - 1])
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return sum(map(ord, s1 + s2)) - dp[-1][-1] * 2
 
 s = Solution()
 

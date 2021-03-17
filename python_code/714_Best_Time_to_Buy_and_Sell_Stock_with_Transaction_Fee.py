70aff3733d1711d7372e42299c8dc0b265ef466c
Guanpeng Gao <gguanpeng@vmware.com>
2021-03-16 22:40:02 -0700

    update 714. Best time to buy and sell stock with transaction

--- python_code/714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.py
+++ python_code/714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.py
@@ -29,6 +29,6 @@ class Solution:
         dp[0][1] = -prices[0]
 
         for i in range(1, len(prices)):
-            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
-            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i] - fee)
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]-fee)
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
         return dp[len(prices)-1][0]
